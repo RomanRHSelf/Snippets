52dda5df1d710d7f13e82e26aea7a955cc202467
RomanRHSelf <romanrh.self@gmail.com>
2024-05-19 15:22:43 +0300

    to step 13 - detail view

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,6 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from MainApp.models import Snippet
 
 def index_page(request):
@@ -18,3 +19,18 @@ def snippets_page(request):
                "snippets_list": snippets_from_db,
                }    
     return render(request, 'pages/view_snippets.html', context)
+
+
+def get_snippet(request, snippet_id:int):
+    try:
+        snippet_from_db = Snippet.objects.get(id=snippet_id)
+        context = {
+            "snippet_id": snippet_from_db.id,
+            "name": snippet_from_db.name,
+            "code": snippet_from_db.code,
+            "lang": snippet_from_db.lang,
+            "creation_date": snippet_from_db.creation_date
+        }
+        return render(request=request, template_name="view_snippet_detail.html", context=context)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f'Товар {snippet_id} не найден')
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index_page, name="snippets-home"),
     path('snippets/add', views.add_snippet_page, name="snippets-add"),
     path('snippets/list', views.snippets_page, name="snippets-list"),
+    path('snippets/<int:snippet_id>/', views.get_snippet, name="snippet-detail"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
