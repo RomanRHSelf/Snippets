d2c8513643209f7eded855d89d0caf21eba83021
RomanRHSelf <romanrh.self@gmail.com>
2024-05-26 17:01:36 +0300

    1-3 задания модуля 8

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea, TextInput
 from MainApp.models import Snippet
 
 class SnippetForm(ModelForm):
     class Meta:
         model = Snippet
         # Описываем поля, которые будем заполнять в форме
-        fields = ['name', 'lang', 'code']
+        fields = ['name', 'lang', 'code', 'private']
+        labels = {"name": "", "lang": "", "code": ""}
+        widgets = {
+            "name": TextInput(attrs={"placeholder": "Название сниппета"}),
+            "code": Textarea(attrs={"placeholder": "Код сниппета"})
+        }
--- MainApp/views.py
+++ MainApp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from MainApp.models import Snippet
 from MainApp.forms import SnippetForm
+from django.contrib import auth
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -20,17 +21,26 @@ def add_snippet_page(request):
     if request.method == "POST":
         form = SnippetForm(request.POST)    
         if form.is_valid():
-            form.save()
+            snippet = form.save(commit=False)
+            if request.user.is_authenticated:
+                snippet.user = request.user
+                snippet.save()
             return redirect("snippets-list")
         return render(request, "pages/add_snippet.html", {'form': form})
 
 def snippets_page(request):
-    snippets_from_db = Snippet.objects.all()
+    snippets_from_db = Snippet.objects.filter(private=True) #all()
     context = {'pagename': 'Просмотр сниппетов',
                "snippets_list": snippets_from_db,
                }    
     return render(request, 'pages/view_snippets.html', context)
 
+def my_snippets_page(request):
+    my_snippets_from_db = Snippet.objects.filter(user=request.user)
+    context = {'pagename': 'Просмотр моих сниппетов',
+               "snippets_list": my_snippets_from_db,
+               }    
+    return render(request, 'pages/view_snippets.html', context)
 
 def get_snippet(request, snippet_id:int):
     try:
@@ -56,6 +66,46 @@ def change_snippet(request, snippet_id:int):
         return render(request=request, template_name="pages/snippet_change.html", context=context)
     except ObjectDoesNotExist:
         return HttpResponseNotFound(f'Товар {snippet_id} не найден')
+
+def snippet_edit(request, snippet_id: int):
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        return Http404
+    
+    # Variant 1
+    # Хотим получить страницу с данными сниппета
+    if request.method == "GET":
+        context = {
+            "pagename": "Редактирование сниппета",
+            "snippet": snippet,
+            "type": "edit"
+        }
+        return render(request, "pages/snippet_detail.html", context)
+    
+    # Variant 2
+    # ==================================================================
+    # Получение сниппета с помощью формы SnippetForm
+    # if request.method == "GET":
+    #     form = SnippetForm(instance=snippet)
+    #     return render(request, "pages/add_snippet.html", {"form": form})
+    # ==================================================================
+
+    # Хотим использовать данные из формы и сохранить изменения в базе
+    if request.method == "POST":
+        data_form = request.POST
+        # Есть экземпляр класса Snippet и новые данные в словаре data_form 
+        # что нужно: взять данные из data_form и заменить ими значения атрибутов экземпляра snippet
+        # как это сделать?
+        snippet.name = data_form["name"]
+        snippet.code = data_form["code"]
+        snippet.private = data_form["private"]
+        if (change_date := data_form.get("creation_date")):
+            snippet.creation_date = change_date
+        # сохраняем этот изменения в базу
+        snippet.save()
+        return redirect("snippets-list")
+
 
 #def create_snippet(request):
  #   if request.method == "POST":
@@ -85,3 +135,26 @@ def save_snippet(request, id:int, name: str, lang: str, code: str):
             return redirect("snippets-list")
         return render(request, "pages/snippet_change.html", {'form': form})
         """
+    
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print("username =", username)
+        print("password =", password)
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+        else:
+            #error msg
+            context = {
+                "pagename": "PythonBin",
+                "errors": ["wrong username or password"]
+            }
+            return render(request, "pages/index.html", context)
+    return redirect("snippets-home") #HttpResponse('check')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('snippets-home')
